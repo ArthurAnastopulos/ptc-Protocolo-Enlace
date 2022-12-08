d2ddae6a76d5eaa2af6a7713dd3b8b232d3d7298
Arthur Anastopulos <arthur.santos@khomp.com>
2022-12-08 11:53:08 -0300

    feat: Desenvolvimento da Arquitetura

--- protocolo-enlace/arquitetura.py
+++ protocolo-enlace/arquitetura.py
@@ -15,13 +15,28 @@ class Arquitetura(Subcamada):
     def recebe(self, quadro):
         print("recebe (Arquitetura)")
         self.__quadro = quadro
+        print("Data do Quadro: ", self.__quadro.data)
+        print("Estado da Arquitetura durante recibimento: ", self.__state)
+        self.__state(self.__quadro)
 
     def envia(self, quadro):
         print("envia (Arquitetura)")
+        print("Estado da Arquitetura durante envio: ", self.__state)
         self.__quadro = quadro
+        self.__filaMsg.put(self.__quadro)
+
+        if self.__quadro.data == "reset":
+            self.__state = self.ocioso ##Muda para estado ocioso
+            self.__quadro = None #limpa o atributo quadro da arquitetura
+        if self.__state == self.ocioso:
+            if self.__filaMsg.qsize() > 1:
+                self.inferior.envia(self.__filaMsg.get()) # !dataN da Modelagem da MEF
+            self.inferior.envia(self.__filaMsgg.get())
+            self.__state = self.espera
 
     def ocioso(self, quadro : Quadro):
         print("ocioso (Arquitetura)")
+        self.disable_timeout()
 
     def espera(self, quadro :  Quadro, tout : bool = False):
         print("espera (Arquitetura)")
--- protocolo-enlace/protocolo.py
+++ protocolo-enlace/protocolo.py
@@ -3,6 +3,7 @@ from subcamada import Subcamada
 from enquadramento import Enquadramento
 from pypoller import poller
 from aplicacao import Aplicacao
+from arquitetura import Arquitetura
 
 class Protocolo(Subcamada):
     def __init__(self, serial, tout):
@@ -10,13 +11,17 @@ class Protocolo(Subcamada):
         self.__tout = tout
         self.__enq = Enquadramento.__init__(port=self.__serial, tout=self.__tout)
         self.__app = Aplicacao.__init__()
-
+        self.__arq = Arquitetura.__init__(tout=self.__tout)
+        
         #Conectar as subcamadas
+        self.__enq.conecta(self.__arq);
+        self.__arq.conecta(self.__app)
 
         # cria o Poller e registra os callbacks
         self.__sched = poller.Poller()
         self.__sched.adiciona(self.__enq)
         self.__sched.adiciona(self.__app)
+        self.__sched.adiciona(self.__arq)
 
     def iniciar(self):
         self.__app.iniciar()
